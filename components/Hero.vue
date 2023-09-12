274114777881971b07635612ce3eb07d2273f794
scottkelly36 <49844516+scottkelly36@users.noreply.github.com>
2023-09-12 22:04:18 +0100

    adds in first components

---
+++ components/Hero.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="TheHero container">
+    <HolidayBuilder />
+    <img  class="TheHero__img" src="/images/the_hero_image.jpg" alt="">
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .TheHero {
+        position: relative;
+        display: flex;
+        padding-top: var(--spacing-2xl);
+        padding-bottom: var(--spacing-lg);
+        height: 700px;
+    }
+
+    .TheHero__img {
+        position: absolute;
+        z-index: 1;
+        right: 0;
+        width: 60%;
+        border-radius: 0.8rem;
+    }
+</style>
---
+++ components/NavBar.vue
@@ -0,0 +1,60 @@
+<template>
+  <nav class="TopNav">
+    <div class="TopNav__container container">
+        <div class="TopNav__logo-container">
+            <img src="/images/logo.png" alt="" class="TopNav__logo"/>
+        </div>
+    
+        <div class="TopNav__navigation-container">
+            <ul class="TopNav__navigation">
+                <li class="TopNav__link">
+                    Home
+                </li>
+                <li class="TopNav__link">
+                    contact
+                </li>
+                <li class="TopNav__link">
+                    FAQ
+                </li>
+            </ul>
+        </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="postcss">
+@import "~/assets/css/settings/vars.css";
+
+    .TopNav {
+        width: 100%;
+        background-color: var(--white);
+        box-shadow: var(--bottom-shadow);
+    }
+
+    .TopNav__container {
+        height: 100px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .TopNav__navigation {
+        display: flex;
+        gap: 1.5rem;
+        list-style: none;
+    }
+
+    .TopNav__logo-container {
+        height: 100%;
+        padding: 1rem 0;
+    }
+    .TopNav__logo {
+        height: 100%;
+    }
+</style>
