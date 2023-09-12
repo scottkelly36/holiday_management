eea5d088ffac4f286f33d86529fa157572055cc6
scottkelly36 <49844516+scottkelly36@users.noreply.github.com>
2023-09-12 22:04:30 +0100

    updates nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
+import { postcss } from "postcss-nested";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  postcss: {
+    plugins: {
+      "postcss-simple-vars": {},
+      "postcss-custom-media": {},
+      "postcss-mixins": {},
+      "postcss-calc": {},
+    },
+  },
+  css: [
+    {
+      lang: 'postcss',
+      src:  '~/assets/css/main.css',
+    }
+  ]
+});
