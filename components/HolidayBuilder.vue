738208e56623ca286d8b3e6468bec5de2a792f3c
scottkelly36 <49844516+scottkelly36@users.noreply.github.com>
2023-09-12 22:06:16 +0100

    adds some post css setup as well as images and font files

---
+++ components/HolidayBuilder.vue
@@ -0,0 +1,33 @@
+<template>
+    <aside class="Form__container">
+        <h2>Build your dream get away!</h2>
+        <p>not sure where to go let us make some suggestions</p>
+        <form action="">
+            <Select>
+                <label for="location">Location</label>
+                <option value="">Anywhere</option>
+            </Select>
+        </form>
+    </aside>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .Form__container {
+        position: absolute;
+        left: 30px;
+        top: 20%;
+        z-index: 4;
+        box-shadow: var(--card-shadow);
+        background-color: var(--white);
+
+        width: 40%;
+        padding: var(--spacing-md);
+        border-radius: 0.8rem;
+    }
+</style>
