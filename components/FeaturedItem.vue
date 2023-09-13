4e642734d465ae6877cf85b867ef91e00370ec62
scottkelly36 <49844516+scottkelly36@users.noreply.github.com>
2023-09-13 21:47:25 +0100

    starts the featured items

---
+++ components/FeaturedItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="FeatureItem">
+    <BaseIcon :use="feature.icon"/>
+    <h3>{{feature.heading}}</h3>
+    <p class="text-small">
+        {{ feature.copy }}
+    </p>
+  </div>
+</template>
+
+<script>
+import BaseIcon from './BaseIcon.vue';
+
+export default {
+    components: {
+        BaseIcon
+    },
+    props: {
+        feature: {
+            type: Object,
+            required: true,
+        }
+    }
+}
+</script>
+
+<style>
+@import '../assets/css/mixins/stack.css';
+
+    .FeatureItem {
+        @mixin stack var(--spacing);
+    }
+</style>
--- components/Features.vue
+++ components/Features.vue
@@ -1,13 +1,44 @@
 <template>
-  
+  <section class="Features container">
+    <template v-for="(feature, index) in this.features" :key='index'>
+        <FeaturedItem :feature="feature" />
+    </template>
+  </section>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            features: [
+                {
+                    icon: 'Shield',
+                    heading: 'Book with confidence',
+                    copy: 'Get 24/7 support and discover helpful reviews from our trusted community of guests.'
+                },
+                {
+                    icon: 'Food',
+                    heading: 'Get the amenities you want',
+                    copy: 'Hot tubs, pools, BBQs – explore dozens of great extras that fit your needs.'
+                },
+                {
+                    icon: 'Calender',
+                    heading: 'Keep it flexible',
+                    copy: 'Stays with flexible cancellation make it easy to rebook if your plans change.'
+                },
+    
+            ]
+        }
+    }, 
 }
 </script>
 
 <style lang="postcss">
+    @import '../assets/css/mixins/row.css';
 
+    .Features {
+        @mixin row var(--spacing-xl);
+        padding-left: 0;
+        padding-right: 0;
+    }
 </style>
