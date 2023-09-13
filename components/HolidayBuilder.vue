96f83addb9cbe59af2a021b595e853a66eb78c60
scottkelly36 <49844516+scottkelly36@users.noreply.github.com>
2023-09-13 13:47:56 +0100

    basic holiday builder form start

--- components/HolidayBuilder.vue
+++ components/HolidayBuilder.vue
@@ -1,12 +1,15 @@
 <template>
-    <aside class="Form__container">
+    <aside class="HolidayForm__container">
         <h2>Build your dream get away!</h2>
         <p>not sure where to go let us make some suggestions</p>
-        <form action="">
-            <Select>
-                <label for="location">Location</label>
+        <form action="" class="Form__container">
+            <label for="location">Location</label>
+            <Select class="FormInput FormInput__select">
                 <option value="">Anywhere</option>
             </Select>
+            <button class="button__outline--primary">
+                Start Your Search
+            </button>
         </form>
     </aside>
 </template>
@@ -18,14 +21,16 @@ export default {
 </script>
 
 <style>
-    .Form__container {
+    .HolidayForm__container {
         position: absolute;
-        left: 30px;
-        top: 20%;
+        display: flex;
+        flex-direction: column;
+        row-gap: 1rem;
+        left: 100px;
+        top: 30%;
         z-index: 4;
         box-shadow: var(--card-shadow);
         background-color: var(--white);
-
         width: 40%;
         padding: var(--spacing-md);
         border-radius: 0.8rem;
