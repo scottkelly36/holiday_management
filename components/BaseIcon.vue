dc003d2afcd60f4688905d31e3f7e4c5b99702bb
scottkelly36 <49844516+scottkelly36@users.noreply.github.com>
2023-09-13 21:46:56 +0100

    adds base icon component and a few components

---
+++ components/BaseIcon.vue
@@ -0,0 +1,28 @@
+<template>
+  <component :is="use" class="icon"/>
+</template>
+
+<script>
+import Calender from './icons/Calender.vue';
+import Shield from './icons/Shield.vue';
+import Food from './icons/Food.vue';
+
+export default {
+    components: {
+        Calender,
+        Shield,
+        Food,
+    },
+    props: {
+        use: {
+            type: String,
+            default: '',
+            required: true,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
