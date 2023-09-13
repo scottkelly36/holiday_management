f764c08e30289579ff4f1351947d6bddfc4e07de
scottkelly36 <49844516+scottkelly36@users.noreply.github.com>
2023-09-13 13:48:25 +0100

    adds plugins

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,13 @@
-import { postcss } from "postcss-nested";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   postcss: {
+    parse: 'postcss-scss',
     plugins: {
       "postcss-simple-vars": {},
       "postcss-custom-media": {},
       "postcss-mixins": {},
       "postcss-calc": {},
+      "postcss-nesting": {},
     },
   },
   css: [
