037dc0cbc43a5cb229830dbcb04991aba78026cb
scottkelly36 <49844516+scottkelly36@users.noreply.github.com>
2023-09-14 23:00:39 +0100

    puts in dummy footer

---
+++ components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="TheFooter">
+
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .TheFooter {
+        height: 50px;
+        background: var(--grey);
+        bottom: 0;
+        margin-top: var(--spacing-3xl);
+    }
+</style>
