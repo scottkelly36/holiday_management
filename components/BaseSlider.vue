56782959812a2d15d159e8d7680d598d6bf4a65e
scottkelly36 <49844516+scottkelly36@users.noreply.github.com>
2023-09-14 23:01:16 +0100

    starts base slider and property card

--- components/BaseSlider.vue
+++ components/BaseSlider.vue
@@ -1,13 +1,74 @@
 <template>
-  
+    <div class="BaseCarousel__container container">
+        <h2>Swiper Title</h2>
+        <p class="text__small">small Description of the swiper images</p>
+        <div ref="carousel" class="swiper-container">
+            <div class="swiper-wrapper">
+                <slot />
+            </div>
+            <div class="BaseCarousel__nav">
+                <button type="button" class="BaseCarousel__nav__btn BaseCarousel__nav__btn--prev">
+                    <BaseIcon use="Chevron"/>
+                </button>
+                <button type="button" class="BaseCarousel__nav__btn BaseCarousel__nav__btn--next">
+                    <BaseIcon use="Chevron"/>
+                </button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
+import Swiper from 'swiper';
 
+export default {
+    props: {
+        carouselSettings: {
+            type: Object, 
+            required: true,
+        }
+    },
+    methods: {
+        initSwiper() {
+            return new Swiper(this.$refs.carousel, {
+                grabCursor: true,
+                slidesPerView: 1,
+                spaceBetween: 30,
+                navigation: {
+                    nextEl: 'BaseCarousel__nav__btn--next',
+                    prevEl: 'BaseCarousel__nav__btn--prev',
+                }
+            })
+        }
+    }
 }
 </script>
 
 <style>
+@import '../assets/css/mixins/stack.css';
 
+    .BaseCarousel__container {
+        @mixin stack var(--spacing);
+    }
+
+    .BaseCarousel__nav {
+        display: flex;
+        gap: var(--spacing);
+    }
+
+    .BaseCarousel__nav__btn {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: var(--grey);
+        height: 2rem;
+        width: 2rem;
+        border: none;
+        color: var(--black);
+        border-radius: 50%;
+    }
+
+    .BaseCarousel__nav__btn--prev {
+        rotate: 180deg;
+    }
 </style>
---
+++ components/PropertyCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="PropertyCard">
+    <div class="PropertyCard__image__container">
+        <img src="" alt="" class="PropertyCard__image">
+    </div>
+    <div class="PropertyCard__text__container">
+        <div class="PropertyCard__text__topLine">
+            <h4 class="PropertyCard__name">
+
+            </h4>
+            <div class="PropertyCard__rating">
+                <BaseIcon use="Star" />
+            </div>
+        </div>
+        <p class="text--small"></p>
+        <div class="PropertyCard__price">
+            <p class="currency"></p>
+            <p class="cost"></p>
+            <p class="frequency"></p>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
