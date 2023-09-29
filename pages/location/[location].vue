a2fd73661fef6d566b7d5d46815bf7bae55537f1
scottkelly36 <49844516+scottkelly36@users.noreply.github.com>
2023-09-29 14:56:37 +0100

    adds some text to the location pages

--- pages/location/[location].vue
+++ pages/location/[location].vue
@@ -1,10 +1,33 @@
 <template>
-  
+    <div class="Location__heading container">
+        <h1>properties in {{location}}</h1>
+    </div>
+    <pageContent/>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            pageContent: [
 
+            ],            
+        }
+    },
+    computed: {
+        location() {
+            const {location} = useRoute().params;
+            const splitLocation = location.split(" ");
+            for(var i =0; i< splitLocation.length; i++) {
+               const capital = splitLocation[i].charAt(0).toUpperCase();
+               const remainingWord = splitLocation[i].slice(1);
+               splitLocation[i] = `${capital}${remainingWord}`;
+            }
+          return splitLocation.join(" ")
+        }
+    },
+
+    
 }
 </script>
 
