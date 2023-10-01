f01cf55afdd93ac9382af1a41b0a91c6b888fa35
scottkelly36 <49844516+scottkelly36@users.noreply.github.com>
2023-10-01 22:07:58 +0100

    create filter component

---
+++ components/Filters.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="Filters">
+
+  </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            filters: [
+                {
+                    name: 'Pool',
+                    icon: 'Pool',
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
