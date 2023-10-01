c0b59a30f0b04349da1abbc4e9ddcf49d520f4bf
scottkelly36 <49844516+scottkelly36@users.noreply.github.com>
2023-10-01 22:18:08 +0100

    starts to add dummy data

--- pages/location/[location].vue
+++ pages/location/[location].vue
@@ -10,7 +10,9 @@ export default {
     data() {
         return {
             pageContent: [
-
+                {
+                    block: 'title'
+                }
             ],            
         }
     },
