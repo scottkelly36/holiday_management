e06e70c7ad8ee4029bbf1f6360e1f6ab9f01df5b
scottkelly36 <49844516+scottkelly36@users.noreply.github.com>
2023-10-01 22:02:43 +0100

    creates the function for redirecting to the chosen location page

--- components/HolidayBuilder.vue
+++ components/HolidayBuilder.vue
@@ -2,11 +2,16 @@
     <aside class="HolidayForm__container">
         <h2>Build your dream get away!</h2>
         <p>not sure where to go let us make some suggestions</p>
-        <form action="" class="Form__container">
-            <Select class="FormInput FormInput__select">
-                <option value="">Anywhere</option>
-            </Select>
-            <button class="button__outline--primary">
+        <form @submit.prevent="redirect" class="Form__container">
+            <select class="FormInput FormInput__select" v-model="selectedLocation">
+                <option value="all">Anywhere</option>
+                <option value="london">London</option>
+                <option value="newcastle">Newcastle</option>
+                <option value="manchester">Manchester</option>
+                <option value="edinburgh">Edinburgh</option>
+                <option value="leeds">Leeds</option>
+            </select>
+            <button type="submit" class="button__outline--primary">
                 Start Your Search
             </button>
         </form>
@@ -15,7 +20,16 @@
 
 <script>
 export default {
-
+    data() {
+        return {
+            selectedLocation: 'all'
+        };
+    },
+    methods: {
+        redirect() {
+            this.$router.push(`/location/${this.selectedLocation}`);
+        }
+    }
 }
 </script>
 
