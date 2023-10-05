4eb7f8c3d156abe09592553c52ff0f53353d1867
scottkelly36 <49844516+scottkelly36@users.noreply.github.com>
2023-10-05 21:40:33 +0100

    adds in icons to base icon

--- components/BaseIcon.vue
+++ components/BaseIcon.vue
@@ -3,33 +3,45 @@
 </template>
 
 <script>
+import Car from './icons/Car.vue';
 import Calender from './icons/Calender.vue';
+import City from './icons/City.vue';
 import Chevron from './icons/Chevron.vue';
 import Facebook from './icons/Facebook.vue';
 import Food from './icons/Food.vue';
 import Insta from './icons/Insta.vue';
 import Minus from './icons/Minus.vue';
+import Music from './icons/Music.vue';
 import Plus from './icons/Plus.vue';
 import Pool from './icons/Pool.vue';
 import Shield from './icons/Shield.vue';
 import Star from './icons/Star.vue';
+import Tree from './icons/Tree.vue';
 import Twitter from './icons/Twitter.vue';
+import Tv from './icons/Tv.vue';
 import TwitterBird from './icons/TwitterBird.vue';
+import Wheelchair from './icons/Wheelchair.vue';
 
 export default {
     components: {
+        Car,
         Calender,
+        City,
         Chevron,
         Facebook,
         Food,
         Insta,
         Minus,
+        Music,
         Plus,
         Pool,
         Shield,
         Star,
+        Tree,
         Twitter,
+        Tv,
         TwitterBird,
+        Wheelchair,
     },
     props: {
         use: {
