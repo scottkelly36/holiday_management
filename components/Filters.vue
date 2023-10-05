b0f37d4df2b8b036a1e46892bf14232136393970
scottkelly36 <49844516+scottkelly36@users.noreply.github.com>
2023-10-05 21:40:52 +0100

    makes filters into buttons

--- components/Filters.vue
+++ components/Filters.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="Filters">
-    <div v-for="(filter, index) in filters" class="Filter__item" :key="index">
+    <button v-for="(filter, index) in filters" class="Filter__item" :key="index">
         <BaseIcon :use="filter.icon" />
         <p class="Filter__item-text">{{filter.name}}</p>
-    </div>
+    </button>
   </section>
 </template>
 
@@ -17,24 +17,28 @@ export default {
                     icon: 'Pool',
                 },
                 {
-                    name: 'Pool',
-                    icon: 'Pool',
+                    name: 'Accessible',
+                    icon: 'Wheelchair',
                 },
                 {
-                    name: 'Pool',
-                    icon: 'Pool',
+                    name: 'TV',
+                    icon: 'Tv',
                 },
                 {
-                    name: 'Pool',
-                    icon: 'Pool',
+                    name: 'Entertainment',
+                    icon: 'Music',
                 },
                 {
-                    name: 'Pool',
-                    icon: 'Pool',
+                    name: 'Rural',
+                    icon: 'Tree',
                 },
                 {
-                    name: 'Pool',
-                    icon: 'Pool',
+                    name: 'City',
+                    icon: 'City',
+                },
+                {
+                    name: 'Transport',
+                    icon: 'Car',
                 },
             ]
         }
@@ -47,14 +51,30 @@ export default {
 @import '../assets/css/mixins/stack.css';
 .Filters{
     @mixin row var(--spacing-md);
+    position: sticky;
+    top: 0;
     max-width: 100vw;
     overflow: scroll;
+    display: flex;
+    justify-content: space-around;
+    background: var(--white);
+    box-shadow: var(--bottom-shadow);
 }
 
 .Filter__item {
     @mixin stack var(--spacing-sm);
     justify-content: center;
     align-items: center;
+    row-gap: 1rem;
+    width: 100px;
+    height: 100px;
+    border: none;
+    background: none;
+}
+
+.Filter__item .icon {
+    width: 30px;
+    height: 30px;
 }
 
 </style>
