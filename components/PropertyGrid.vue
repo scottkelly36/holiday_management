c9b17ad92194eda484399819ae40ee67aec13e16
scottkelly36 <49844516+scottkelly36@users.noreply.github.com>
2023-10-05 21:41:07 +0100

    creates property grid

---
+++ components/PropertyGrid.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="PropertyGrid container">
+    <div v-for="(property, index) in properties" :key="index" class="PropertyCard__container">
+        <PropertyCard :content="property"/>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+    props: {
+        properties: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+    .PropertyGrid {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+    }
+</style>
